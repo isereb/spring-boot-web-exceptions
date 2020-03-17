3c04a83f7f4cde64ebbdc5582164a2663060ac03
isereb <ruxxay.coder@gmail.com>
2020-03-17 04:08:04 +0000

    Annotated companion object funs with @JvmStatic for better api

--- src/main/kotlin/com/isereb/web/exceptions/WebErrors.kt
+++ src/main/kotlin/com/isereb/web/exceptions/WebErrors.kt
@@ -5,13 +5,13 @@ import org.springframework.http.HttpStatus
 class WebErrors {
     companion object {
 
-        fun notFound(message: String) =
+        @JvmStatic fun notFound(message: String) =
                 SimpleWebError(HttpStatus.NOT_FOUND, message)
 
-        fun badRequest(message: String) =
+        @JvmStatic fun badRequest(message: String) =
                 SimpleWebError(HttpStatus.BAD_REQUEST, message)
 
-        fun badRequest(message: String, description: String, vararg fields: ErroneousField) =
+        @JvmStatic fun badRequest(message: String, description: String, vararg fields: ErroneousField) =
                 ValidationWebError(HttpStatus.BAD_REQUEST, message, description, fields)
     }
 }
