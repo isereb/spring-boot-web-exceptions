1d842e7aee33b5aaa1fb39905c9274a1385302eb
isereb <ruxxay.coder@gmail.com>
2020-03-17 02:22:48 +0000

    Version 1.0.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 group = "com.github.isereb"
-version = "1.0.0"
+version = "1.0.2"
 
 plugins {
     id("org.springframework.boot") version "2.2.5.RELEASE"
