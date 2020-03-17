a3f783b4f70ad11ef46e4ce1dec5db01268ae88b
isereb <ruxxay.coder@gmail.com>
2020-03-17 01:48:22 +0000

    Added maven-publish plugin instead of maven

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
     id("io.spring.dependency-management") version "1.0.9.RELEASE"
     kotlin("jvm") version "1.3.61"
     kotlin("plugin.spring") version "1.3.61"
-    maven
+    `maven-publish`
 }
 
 java.sourceCompatibility = JavaVersion.VERSION_1_8
