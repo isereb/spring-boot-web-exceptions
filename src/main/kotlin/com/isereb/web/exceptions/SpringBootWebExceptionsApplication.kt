b3598299ce0c9663eb53b4aa16900bb57ed1366e
isereb <ruxxay.coder@gmail.com>
2020-03-17 00:20:00 +0000

    first commit

---
+++ src/main/kotlin/com/isereb/web/exceptions/SpringBootWebExceptionsApplication.kt
@@ -0,0 +1,11 @@
+package com.isereb.web.exceptions
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBootWebExceptionsApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringBootWebExceptionsApplication>(*args)
+}
---
+++ src/test/kotlin/com/isereb/web/exceptions/SpringBootWebExceptionsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.isereb.web.exceptions
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringBootWebExceptionsApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
