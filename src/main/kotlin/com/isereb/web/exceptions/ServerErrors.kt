819d633b59257c25b7c058cc1a9f1e1d68e58d7e
isereb <ruxxay.coder@gmail.com>
2020-03-31 02:58:46 +0000

    Added overloaded methods for server error exception factory methods

--- src/main/kotlin/com/isereb/web/exceptions/ServerErrors.kt
+++ src/main/kotlin/com/isereb/web/exceptions/ServerErrors.kt
@@ -6,14 +6,27 @@ import org.springframework.http.HttpStatus
 class ServerErrors {
 
     companion object {
+
+        @JvmStatic fun internalError(message: String) =
+                SimpleWebError(HttpStatus.INTERNAL_SERVER_ERROR, message, null)
+
         @JvmStatic fun internalError(message: String, description: String?) =
                 SimpleWebError(HttpStatus.INTERNAL_SERVER_ERROR, message, description)
+
+        @JvmStatic fun notImplemented(message: String) =
+                SimpleWebError(HttpStatus.NOT_IMPLEMENTED, message, null)
 
         @JvmStatic fun notImplemented(message: String, description: String?) =
                 SimpleWebError(HttpStatus.NOT_IMPLEMENTED, message, description)
 
+        @JvmStatic fun badGateway(message: String) =
+                SimpleWebError(HttpStatus.BAD_GATEWAY, message, null)
+
         @JvmStatic fun badGateway(message: String, description: String?) =
                 SimpleWebError(HttpStatus.BAD_GATEWAY, message, description)
+
+        @JvmStatic fun serviceUnavailable(message: String) =
+                SimpleWebError(HttpStatus.SERVICE_UNAVAILABLE, message, null)
 
         @JvmStatic fun serviceUnavailable(message: String, description: String?) =
                 SimpleWebError(HttpStatus.SERVICE_UNAVAILABLE, message, description)
